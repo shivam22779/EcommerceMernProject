4b3039354aaebe7172e8f5a3d68e6ffed591ed2a
Shivam Singh <shivamsingh201516@gmail.com>
2022-09-18 11:25:40 +0530

    bug fix in payment page

--- frontened/src/App.js
+++ frontened/src/App.js
@@ -78,6 +78,15 @@ function App() {
     <Router>
       <Header />
       {isAuthenticated && <UserOptions user={user} />}
+      {window.location.pathname === "/process/payment" && (
+        <ProtectedRoute>
+          {stripeApiKey && (
+            <Elements stripe={loadStripe(stripeApiKey)}>
+              <Payment />
+            </Elements>
+          )}
+        </ProtectedRoute>
+      )}
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/contact" element={<Contact />} />
@@ -135,7 +144,7 @@ function App() {
           }
         />
 
-        {stripeApiKey && (
+        {/* {stripeApiKey && (
           <Route
             path="/process/payment"
             element={
@@ -146,7 +155,7 @@ function App() {
               </ProtectedRoute>
             }
           />
-        )}
+        )} */}
 
         <Route
           path="/success"
@@ -244,7 +253,14 @@ function App() {
             </AdminRoute>
           }
         />
-        <Route path="/*" element={<PageNotFound />} />
+        <Route
+          path="/*"
+          element={
+            window.location.pathname === "/process/payment" ? null : (
+              <PageNotFound />
+            )
+          }
+        />
       </Routes>
       <Footer />
     </Router>
--- frontened/src/components/pageNotFound/PageNotFound.js
+++ frontened/src/components/pageNotFound/PageNotFound.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import './PageNotFound.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./PageNotFound.css";
 
 const PageNotFound = () => {
   return (
-    <div className='pageContainer'>
-        <div>
-      <h2>Oops! Page not found</h2>
-      <h1>404</h1>
-      <p>We can't find the page you are looking for...</p>
-      
-      <Link to="/">Go Back to home</Link>
-      </div>
+    <div className="pageContainer">
+      <div>
+        <h2>Oops! Page not found</h2>
+        <h1>404</h1>
+        <p>We can't find the page you are looking for...</p>
 
+        <Link to="/">Go Back to home</Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default PageNotFound;
